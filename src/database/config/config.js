b296480dfb3f35d1f46e584ab527dd080dc42a98
matias798 <matiquiro.mq@gmail.com>
2021-04-21 16:13:21 -0300

    added error page view

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
 module.exports={
   "development": {
-      "username": "b0ce2c4eb0499f",
-  "password": "5a1e9112",
-  "database": "heroku_d395cdae7c166d3",
-  "host":"us-cdbr-east-03.cleardb.com" ,
+  "username": "root",
+  "password": "password",
+  "database": "rame_deco_db",
+  "host":"127.0.0.1" ,
   "dialect": "mysql"
 
   },
